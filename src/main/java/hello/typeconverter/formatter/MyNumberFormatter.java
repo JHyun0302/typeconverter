bb72a9f3cfe4276c195565d1a3904a2cd2ea4435
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:11:41 +0900

    Formatter: 문자에 특화(문자->객체, 객체->문자) + 현지화(Locale)

--- src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
+++ src/main/java/hello/typeconverter/formatter/MyNumberFormatter.java
@@ -7,10 +7,16 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.Locale;
 
+/**
+ * Formatter: 문자에 특화(문자->객체, 객체->문자) + 현지화(Locale)
+ * ex) 숫자(1000) -> 문자("1000")
+ * <p>
+ * test: http://localhost:8080/hello-v2?data=1,000
+ */
 @Slf4j
 public class MyNumberFormatter implements Formatter<Number> {
     @Override
-    public Number parse(String text, Locale locale) throws ParseException { //문자 -> 숫자
+    public Number parse(String text, Locale locale) throws ParseException { //문자 -> 객체
         log.info("text={}, locale={}", text, locale);
         //"1,000"->1000
         NumberFormat format = NumberFormat.getInstance(locale);
@@ -18,7 +24,7 @@ public class MyNumberFormatter implements Formatter<Number> {
     }
 
     @Override
-    public String print(Number object, Locale locale) { // 숫자 객체 -> 문자
+    public String print(Number object, Locale locale) { // 객체 -> 문자
         log.info("object={}, locale={}", object, locale);
         return NumberFormat.getInstance(locale).format(object);
     }
