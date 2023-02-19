0f51d236a31fcdaebe073077a691d5ec1e00ab83
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:10:08 +0900

    웹 애플리케이션에 Converter 적용

--- src/main/java/hello/typeconverter/WebConfig.java
+++ src/main/java/hello/typeconverter/WebConfig.java
@@ -7,11 +7,14 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+/**
+ * 웹 애플리케이션에 Converter 적용
+ */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addFormatters(FormatterRegistry registry) {
-        //주석처리 우선순위
+        //주석처리 우선순위: MyNumberFormatter()와 기능이 겹침(숫자 -> 문자, 문자 -> 숫자)
 //        registry.addConverter(new StringToIntegerConverter());
 //        registry.addConverter(new IntegerToStringConverter());
         registry.addConverter(new StringToIpPortConverter());
