ff2eb83bd4444ff1dd86ba760af7bfedd491bbde
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:10:01 +0900

    컨버터 인터페이스

--- src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
+++ src/main/java/hello/typeconverter/converter/StringToIntegerConverter.java
@@ -3,9 +3,12 @@ package hello.typeconverter.converter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
+/**
+ * 컨버터 인터페이스
+ * String -> Integer
+ */
 @Slf4j
 public class StringToIntegerConverter implements Converter<String, Integer> {
-    //String -> Integer
     @Override
     public Integer convert(String source) {
         log.info("convert source={}", source);
