c50de8c6956523df715bd1bc7c59650970333236
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:10:23 +0900

    컨버터 인터페이스

--- src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
+++ src/main/java/hello/typeconverter/converter/IntegerToStringConverter.java
@@ -3,9 +3,12 @@ package hello.typeconverter.converter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
+/**
+ * 컨버터 인터페이스
+ * Integer->String
+ */
 @Slf4j
 public class IntegerToStringConverter implements Converter<Integer, String> {
-    //Integer->String
     @Override
     public String convert(Integer source) {
         log.info("convert source={}", source);
--- src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
+++ src/main/java/hello/typeconverter/converter/IpPortToStringConverter.java
@@ -4,13 +4,16 @@ import hello.typeconverter.type.IpPort;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.convert.converter.Converter;
 
+/**
+ * 컨버터 인터페이스
+ * IpPort 객체 -> "127.0.0.1:8080"
+ */
 @Slf4j
 public class IpPortToStringConverter implements Converter<IpPort, String> {
 
     @Override
     public String convert(IpPort source) {
         log.info("convert source={}", source);
-        //IpPort 객체 -> "127.0.0.1:8080"
         return source.getIp() + ":" + source.getPort();
     }
 }
