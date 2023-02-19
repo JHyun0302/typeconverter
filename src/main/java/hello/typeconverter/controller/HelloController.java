b61e91a0e4704db4707e5d7da0b1dea3189bdcef
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:10:36 +0900

    타입을 변환해야하는 경우 기본 예시

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 타입을 변환해야하는 경우 기본 예시
+ */
 @RestController
 public class HelloController {
     @GetMapping("/hello-v1")
@@ -17,12 +20,13 @@ public class HelloController {
         return "ok";
     }
 
+    //http://localhost:8080/hello-v2?data=10000
     @GetMapping("/hello-v2") //문자 10000 -> 숫자 10000
     public String helloV2(@RequestParam Integer data) {
         System.out.println("data = " + data);
         return "ok";
     }
-
+    
     @GetMapping("/ip-port") //localhost:8080/ip-port?ipPort=127.0.0.1:8080
     public String ipPort(@RequestParam IpPort ipPort) {
         System.out.println("ipPort Ip = " + ipPort.getIp());
