a81ca1f0b873fb7eff440f2c8a502a122da48c3d
jaehyun <wogustkfkd11@gmail.com>
2023-02-19 23:10:57 +0900

    뷰 템플릿에 컨버터를 적용

--- src/main/java/hello/typeconverter/controller/ConverterController.java
+++ src/main/java/hello/typeconverter/controller/ConverterController.java
@@ -8,6 +8,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+/**
+ * 뷰 템플릿에 컨버터를 적용
+ * <p>
+ * 1. 타임리프(${{..}}) - 자동으로 컨버전 서비스 사용해서 변환된 결과 출력
+ * 2. th:field: 컨버전 서비스 적용, th:value: 컨버전 서비스 적용 X
+ */
 @Controller
 public class ConverterController {
     @GetMapping("/converter-view")
